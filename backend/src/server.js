be6f0c2d775b4ccfde07860fc74e81c977377f86
Michal <michaustosio@gmail.com>
2019-10-17 22:16:43 +0200

    eslint config for backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import { connect } from './utils/db'
-import { config } from './config'
-import placesRouter from './resources/places/places.router'
+import express from 'express';
+import cors from 'cors';
+import { connect } from './utils/db';
+import { config } from './config';
+import placesRouter from './resources/places/places.router';
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.disable('x-powered-by')
-app.use(cors())
-app.use(express.json())
+app.disable('x-powered-by');
+app.use(cors());
+app.use(express.json());
 
-app.use('/', placesRouter)
+app.use('/', placesRouter);
 
 export const start = async () => {
-  try {
-    await connect()
-    app.listen(config.port, (req, res) => {
-      console.log(`Server running at ${config.port}`)
-    })
-  } catch (e) {
-    console.log(e)
-  }
-}
+	try {
+		await connect();
+		app.listen(config.port, (req, res) => {
+			console.log(`Server running at ${config.port}`);
+		});
+	} catch (e) {
+		console.log(e);
+	}
+};
---
+++ frontend/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	semi: false,
+	singleQuote: true,
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 4,
+	trailingComma: 'all',
+}
