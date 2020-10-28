c35c6eaa15661789e27b1f400663c8b0a6cc5a2e
Anna Konopka <kernelgonnapanic@gmail.com>
2020-10-28 17:30:54 +0100

    Add test cases

---
+++ frontend/src/pages/AddNewPlace/AddNewPlaceForm.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+describe('AddNewPlace', () => {
+	xit('renders', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('a notification is shown after successful place creation', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('an error is shown when there was an error while creating the place', () => {
+		throw new Error('Not implemented')
+	})
+
+	// form related tests
+	xit('lat and lng can be chosen by dragging the marker on the map', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('changing lat and lng moves the marker on the map', () => {
+		// this is not implemented in the app but without that the map looks broken ;)
+		throw new Error('Not implemented')
+	})
+
+	xit('onSubmit handler is called when form is valid and submit button was clicked', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('shows field errors when one attempts to submit empty form', () => {
+		throw new Error('Not implemented')
+	})
+})
