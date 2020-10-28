34fc5c4386226cf5f8f28dba1480275a18e73f9a
Anna Konopka <kernelgonnapanic@gmail.com>
2020-10-28 18:40:04 +0100

    Add test cases for usePlaces hook

---
+++ frontend/src/assets/hooks/usePlaces.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+describe('usePlaces', () => {
+	xit('return placesLoading = true when data is being fetched', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('return hasMoreData = true when there is more data to be fetched', () => {
+		throw new Error('Not implemented')
+	})
+
+	xit('when page number changes loads more data', () => {
+		throw new Error('Not implemented')
+	})
+})
